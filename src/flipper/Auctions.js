e8d0abb15c141e4400bb5f18a7f31fe507110153
Vertixx <skemproof2@gmail.com>
2022-12-04 20:41:57 +0530

    made some changes

--- src/flipper/Auctions.js
+++ src/flipper/Auctions.js
@@ -1,10 +1,10 @@
 class Auctions {
     constructor() {
-        this.MAX = 500000;
+        this.MAX = 500000000;
         this.MIN = 5;
-        this.multiplier = 0.85;
+        this.multiplier = 0.84;
         this.margin = 3;
-        this.profit = 50000;
+        this.profit = 2000000;
         this.auctions = [];
         this.totalAuctions = 0;
         this.filteredAuctions = [];
@@ -23,6 +23,7 @@ class Auctions {
                     currentPage++;
                     if (currentPage == pages.totalPages) {
                         done = true;
+                        console.log(`Total pages: ${pages.totalPages}`);
                     }
                 });
             }));
@@ -61,32 +62,33 @@ class Auctions {
         });
         for (const item in items) {
             var cheapest = [];
+            var tier = [];
             if (items[item].length >= 10) {
                 items[item].forEach(auction => {
-                    cheapest.push(auction.starting_bid);
+                    cheapest.push({ price: auction.starting_bid, tier: auction.tier });
                 });
-                cheapest.sort((a, b) => a - b);
+                cheapest.sort((a, b) => a.price - b.price);
                 cheapest = cheapest.slice(0, 2);
                 if (cheapest.length == 2) {
-                    const differnce = Math.round(cheapest[1] * this.multiplier - cheapest[0]);
-                    const margin = Math.round(differnce / cheapest[0] * 100);
-                    if (differnce >= this.profit && margin >= this.margin && items[item][0].tier == items[item][1].tier) {
+                    const differnce = Math.round(cheapest[1].price * this.multiplier - cheapest[0].price);
+                    const margin = Math.round(differnce / cheapest[0].price * 100);
+                    if (differnce > this.profit && margin > this.margin && cheapest[0].tier == cheapest[1].tier) {
                         this.finalAuctions.push({
                             name: items[item][0].item_name,
                             tier: items[item][0].tier,
-                            price: cheapest[0],
-                            price2: cheapest[1],
+                            price: cheapest[0].price,
+                            price2: cheapest[1].price,
                             difference: differnce,
                             margin: margin,
                             ahid: items[item][0].uuid,
                             lore: items[item][0].item_lore
                         });
                     }
-                    this.finalAuctions.sort((a, b) => a.margin - b.margin);
                 }
+                this.finalAuctions.sort((a, b) => a.margin - b.margin);
             }
         }
-        
+        console.log(`Total auctions: ${this.finalAuctions.length}`);
         this.done = true;
         return new Promise((resolve, reject) => {
             const interval = setInterval(() => {
@@ -98,5 +100,6 @@ class Auctions {
         });
     }
 }
+
 
 module.exports = function () { return new Auctions(); };
